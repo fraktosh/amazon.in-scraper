4d9dd6e418cd5dda76b72274b3e0455f891828f5
Soham Saha <34938615+fraktosh@users.noreply.github.com>
2023-03-09 04:53:57 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,6 @@
+import finder
+import time
+cat_list = ['amazon-renewed', 'apparel' , 'baby' ,'luggage','beauty','computers','electronics', 'garden','grocery','hpc','kitchen','home-improvement','industrial','jewelry', 'musical-instruments','office','pet-supplies','shoes','toys','watches']
+x=(int)(input('no.'))
+af=cat_list[x]
+exec('finder')
---
+++ newtwo.py
@@ -0,0 +1,3 @@
+import new
+for i in range(0,20):
+    new.fuck(i)
