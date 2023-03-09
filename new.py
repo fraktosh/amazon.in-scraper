3a08cd0d4a373add1721fc0d246d504d722cd2fa
Soham Saha <34938615+fraktosh@users.noreply.github.com>
2023-03-09 19:30:11 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-timport finder
+import finder
 import time
 cat_list = ['amazon-renewed', 'apparel' , 'baby' ,'luggage','beauty','computers','electronics', 'garden','grocery','hpc','kitchen','home-improvement','industrial','jewelry', 'musical-instruments','office','pet-supplies','shoes','toys','watches']
 x=(int)(input('enter a number from 0 to 19'))
